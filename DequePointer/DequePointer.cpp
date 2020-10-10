489cfe5a9633d47a102fd50865ec5043471ceab2
Welbeden <43912308+Welbeden@users.noreply.github.com>
2020-10-11 00:02:10 +0300

    Add files via upload

--- DequePointer/DequePointer.cpp
+++ DequePointer/DequePointer.cpp
@@ -1,6 +1,7 @@
-﻿#include <iostream>
+﻿#include <iostream> 
 #include <string>
 using namespace std;
+bool IsEmpty = true;
 struct Item
 {
 	int item;
@@ -10,19 +11,20 @@ struct Item
 struct Deque
 {
 	int AllSize = 0;
-	Item* LeElem;
-	Item* RiElem;
+	Item* LeElem = NULL;
+	Item* RiElem = NULL;
 };
-void Inicialization(Deque* el)
+void CommandList()
 {
-	int num;
-	cout << "Type first item for the first node: ";
-	cin >> num;
-	el->LeElem = new Item;
-	el->LeElem->next = nullptr;
-	el->LeElem->prev = nullptr;
-	el->LeElem->item = num;
-	el->RiElem = el->LeElem;
+	cout << "Available commands: " << endl << endl;
+	cout << "1. Create left node" << endl;
+	cout << "2. Create right node" << endl;
+	cout << "3. Show current deque size" << endl;
+	cout << "4. Show all nodes" << endl;
+	cout << "5. Delete right node" << endl;
+	cout << "6. Delete left node" << endl;
+	cout << "7. Delete all nodes" << endl;
+	cout << "8. Exit program" << endl;
 }
 void CreateRightNode(Deque* el)
 {
@@ -30,15 +32,25 @@ void CreateRightNode(Deque* el)
 	int num;
 	cout << "Type number for deque: ";
 	cin >> num;
-	if ((el->RiElem->next == nullptr and el->RiElem->prev == nullptr) or el->RiElem->next == nullptr) {
+	if (IsEmpty == false) {
 		helpR = el->RiElem;
 		el->RiElem = new Item;
 		helpR->next = el->RiElem;
 		el->RiElem->prev = helpR;
 		el->RiElem->next = nullptr;
 		el->RiElem->item = num;
-		cout << "Node created" << endl;
+		cout << "Right node created" << endl;
 		el->AllSize++;
+	}
+	else if (IsEmpty != false) {
+		el->RiElem = new Item;
+		el->RiElem->next = nullptr;
+		el->RiElem->prev = nullptr;
+		el->RiElem->item = num;
+		el->LeElem = el->RiElem;
+		el->AllSize++;
+		IsEmpty = false;
+		cout << "Right node created" << endl;
 	}
 }
 void CreateLeftNode(Deque* el)
@@ -47,15 +59,25 @@ void CreateLeftNode(Deque* el)
 	int num;
 	cout << "Type number for deque: ";
 	cin >> num;
-	if ((el->LeElem->next == nullptr and el->LeElem->prev == nullptr) or el->LeElem->prev == nullptr) {
+	if (IsEmpty == false) {
 		helpL = el->LeElem;
 		el->LeElem = new Item;
 		helpL->prev = el->LeElem;
 		el->LeElem->next = helpL;
 		el->LeElem->prev = nullptr;
 		el->LeElem->item = num;
-		cout << "Node created" << endl;
+		cout << "Left node created" << endl;
 		el->AllSize++;
+	}
+	else if (IsEmpty != false) {
+		el->LeElem = new Item;
+		el->LeElem->next = nullptr;
+		el->LeElem->prev = nullptr;
+		el->LeElem->item = num;
+		el->RiElem = el->LeElem;
+		el->AllSize++;
+		IsEmpty = false;
+		cout << "Left node created" << endl;
 	}
 }
 void DequeSize(Deque* el)
@@ -84,10 +106,14 @@ void DeleteRightNode(Deque* el)
 		el->RiElem = helpR;
 		helpR = helpR->prev;
 		el->RiElem->next = nullptr;
+		cout << endl << "Right node deleted" << endl;
 		el->AllSize--;
 	}
 	else if (el->RiElem->prev == nullptr) {
 		delete el->RiElem;
+		IsEmpty = true;
+		el->LeElem = el->RiElem;
+		el->AllSize--;
 		cout << endl << "Right node deleted" << endl;
 	}
 	else cout << "No nodes" << endl << endl;
@@ -101,17 +127,21 @@ void DeleteLeftNode(Deque* el)
 		el->LeElem = helpL;
 		helpL = helpL->next;
 		el->LeElem->prev = nullptr;
+		cout << endl << "Left node deleted" << endl;
 		el->AllSize--;
 	}
 	else if (el->LeElem->next == nullptr) {
 		delete el->LeElem;
+		IsEmpty = true;
+		el->RiElem = el->LeElem;
+		el->AllSize--;
 		cout << endl << "Left node deleted" << endl;
 	}
 	else cout << "No nodes" << endl << endl;
 }
 void DeleteAll(Deque* el)
 {
-		while (el->LeElem->prev == nullptr && el->RiElem->next != nullptr)
+		while (el->LeElem->next != nullptr)
 		{
 			Item* helpL = el->LeElem;
 			helpL = el->LeElem->next;
@@ -122,28 +152,20 @@ void DeleteAll(Deque* el)
 			if (el->AllSize != 0)
 				el->AllSize--;
 		}
-		if ((el->LeElem->prev == nullptr and el->LeElem->next == nullptr)
-			or (el->RiElem->next == nullptr and el->RiElem->prev == nullptr))
-			delete el->LeElem;
+		delete el->LeElem;
+		IsEmpty = true;
+		el->AllSize--;
 }
 int main()
 {
 	Deque node;
 	int command;
-	Inicialization(&node);
-	cout << "Available commands: " << endl << endl;
-	cout << "1. Create left node" << endl;
-	cout << "2. Create right node" << endl;
-	cout << "3. Show current deque size" << endl;
-	cout << "4. Show all nodes" << endl;
-	cout << "5. Delete right node" << endl;
-	cout << "6. Delete left node" << endl;
-	cout << "7. Exit program" << endl;
+	CommandList();
 	while (1)
 	{
 		cout << endl << "Type command for deque: " << endl;
 		cin >> command;
-		if (command >= 1 && command < 7)
+		if (command >= 1 && command < 4)
 			switch (command) {
 			case 1:
 				CreateLeftNode(&node);
@@ -155,18 +177,21 @@ int main()
 				DequeSize(&node);
 				break;
 			}
-		else if (command == 7 and node.LeElem->item) {
+		else if (command == 8 and IsEmpty == true) 
+			break;
+		else if (command == 8 and IsEmpty != true) {
 			DeleteAll(&node);
 			break;
 		}
-		else if (command == 6 and node.LeElem->item) {
+		else if (command == 7 and IsEmpty != true)
+			DeleteAll(&node);
+		else if (command == 6 and IsEmpty != true) 
 			DeleteLeftNode(&node);
-		}
-		else if (command == 5 and node.LeElem->item)
+		else if (command == 5 and IsEmpty != true)
 			DeleteRightNode(&node);
-		else if (command == 4 and node.LeElem->item)
+		else if (command == 4 and IsEmpty != true)
 			ShowAllNodes(&node);
-		else cout << "Incorrect command" << endl;
+		else cout << "Error. Type number of another command again" << endl;
 	}
 	return 0;
 }
